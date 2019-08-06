281dcc746c052e009e54e7fff1f781e1e026c2d2
st8324 <stajun@naver.com>
2019-08-06 17:56:50 +0900

    1일차 수업 내용

---
+++ src/day1/Hello.java
@@ -0,0 +1,33 @@
+﻿package day1;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		int num1 = 10;
+		int num2 = 0b10;
+		int num3 = 010;
+		int num4 = 0x10;
+		System.out.println("num1 = "+num1);
+		System.out.println("num2 = "+num2);
+		System.out.println("num3 = "+num3);
+		System.out.println("num4 = "+num4);
+		char ch1 = 'A';
+		char ch2 = 65;
+		char ch3 = '\u0041';//41 => 16*4 + 1 * 1 = 65
+		System.out.println("ch1 = " + ch1);
+		System.out.println("ch2 = " + ch2);
+		System.out.println("ch3 = " + ch3);
+		// =을 기준으로 왼쪽과 오른쪽이 모두 정수형일 때
+		// 왼쪽의 메모리 크기가 크면 자동 자료형(타입) 변환이 된다.
+		int n = 'A';
+		
+		// =을 기준으로 왼쪽이 실수이고 오른쪽이 정수일 때
+		// 자동 자료형 변환이 된다.
+		double d = 1;
+		d = 1.23;
+		//Type mismatch: cannot convert from 
+		//A to B
+		int n1 = (int)d;
+	}
+
+}
---
+++ src/day1/Scan.java
@@ -0,0 +1,27 @@
+﻿package day1;
+
+import java.util.Scanner;
+
+public class Scan {
+
+	public static void main(String[] args) {
+		//Scanner라는 설계도를 이용하여 scanner이라는 이름을
+		//가진 제품을 선언하고 new라는 연산자를 이용하여
+		//제품을 생성
+		//콘솔에서 입력받기 위해 System.in을 사용
+		Scanner scanner = new Scanner(System.in);
+		
+		int num1 = scanner.nextInt();
+		double num2 = scanner.nextDouble();
+		System.out.println(num1);
+		System.out.println(num2);
+		//next()라는 문자열을 읽어오는 메소드를 이용하여 문자열을
+		//읽어온 후 charAt()메소드를 이용하여 0번지에 있는 값만
+		//가져온다.
+		char ch = scanner.next().charAt(0);
+		System.out.println(ch);
+		System.out.println(1 + '+' + 2);
+		scanner.close();
+	}
+
+}
